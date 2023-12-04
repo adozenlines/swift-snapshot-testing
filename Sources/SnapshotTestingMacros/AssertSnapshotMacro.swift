e32847cace262c13e7797655d874ab194501b92e
sean batson <3752237+adozenlines@users.noreply.github.com>
2023-12-04 00:02:49 -0500

    Refactored - Removed redundant argument logic

--- Sources/SnapshotTestingMacros/AssertSnapshotMacro.swift
+++ Sources/SnapshotTestingMacros/AssertSnapshotMacro.swift
@@ -4,59 +4,6 @@ import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 import SwiftDiagnostics
 
-struct AssertSnapShotParam {
-    
-    /// The macro's parameters for extraction
-    let param: AssertSnapShotParamList
-    
-    /// The extracted argument list parameters and expressions from the syntax tree
-    let node: LabeledExprListSyntax
-    
-    /// The extracted expressions by parameter
-    var value: ExprSyntax? {
-        for child in node.children(viewMode: .all) {
-            switch param {
-            case .as:
-                if child.as(LabeledExprSyntax.self)?.label?.trimmed.text == param.rawValue {
-                    return child.as(LabeledExprSyntax.self)?.expression
-                } else {
-                   continue
-                }
-            case .of:
-                if child.as(LabeledExprSyntax.self)?.label?.trimmed.text == param.rawValue {
-                    return child.as(LabeledExprSyntax.self)?.expression
-                } else {
-                    continue
-                }
-            case .named:
-                if child.as(LabeledExprSyntax.self)?.label?.trimmed.text == param.rawValue {
-                    return child.as(LabeledExprSyntax.self)?.expression
-                } else {
-                    continue
-                }
-            case .record:
-                if child.as(LabeledExprSyntax.self)?.label?.trimmed.text == param.rawValue {
-                    return child.as(LabeledExprSyntax.self)?.expression
-                } else {
-                    continue
-                }
-            case .timeout:
-                if child.as(LabeledExprSyntax.self)?.label?.trimmed.text == param.rawValue {
-                    return child.as(LabeledExprSyntax.self)?.expression
-                } else {
-                    continue
-                }
-            }
-        }
-        return nil
-    }
-}
-
-/// The `assertSnapshot` parameter labels
-enum AssertSnapShotParamList: String {
-   case `as`, `of`, named, record, timeout
-}
-
 /// Implementation of the `AssertSnapshot` macro
 ///
 ///     #AssertSnapshotEqual(of: "Sample", as: .lines)
@@ -70,20 +17,6 @@ public struct AssertSnapshotEqualMacro: ExpressionMacro {
     /// - Returns: The expression syntax representation of the function call.
     public static func expansion(of node: some SwiftSyntax.FreestandingMacroExpansionSyntax, in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> SwiftSyntax.ExprSyntax {
         guard !node.argumentList.children(viewMode: .all).isEmpty else {
-            throw AssertSnapshotEqualError.message("AssertSnapshotEqual - Macro: the macro was not passed any arguments")
-        }
-        
-        let ofArg = AssertSnapShotParam(param: .of, node: node.argumentList).value
-        let asArg = AssertSnapShotParam(param: .as, node: node.argumentList).value
-        let nameArg = AssertSnapShotParam(param: .named, node: node.argumentList).value
-        let recordArg = AssertSnapShotParam(param: .record, node: node.argumentList).value
-        let timeoutArg = AssertSnapShotParam(param: .timeout, node: node.argumentList).value
-
-        if let nameArg, let ofArg, let asArg, let recordArg, let timeoutArg {
-            return "assertSnapshot(of: \(ofArg), as: \(asArg), named: \(nameArg), record: \(recordArg), timeout: \(timeoutArg))"
-        } else if let ofArg, let asArg {
-            return "assertSnapshot(of: \(ofArg), as: \(asArg))"
-        } else {
             context.diagnose(
                   Diagnostic(
                     node: Syntax(node),
@@ -94,8 +27,10 @@ public struct AssertSnapshotEqualMacro: ExpressionMacro {
                     )
                   )
                 )
-            throw AssertSnapshotEqualError.message("AssertSnapshotEqual - Macro: the macro does not have any of the expected arguments")
+            throw AssertSnapshotEqualError.message("AssertSnapshotEqual - Macro: the macro was not passed any arguments")
         }
+        // Pass the incoming macro signature to the generated assertSnapshot(...) expression
+        return "assertSnapshot(\(node.argumentList))"
     }
 }
 
